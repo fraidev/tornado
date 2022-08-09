ee88bfc99c70e55da3ca0ed524459c7f505edd33
Felipe Cardozo <fraifelipe@gmail.com>
2022-08-09 04:41:36 -0300

    fix torrent.ml fields

--- lib/torrent.ml
+++ lib/torrent.ml
@@ -3,9 +3,9 @@ open Bencode_utils
 module File = struct
   type t =
     { announce : string option
-    ; infoHash : bytes
-    ; pieceHashes : bytes array
-    ; pieceLength : int64 option
+    ; info_hash : bytes
+    ; piece_hashes : bytes array
+    ; piece_length : int64 option
     ; length : int64 option
     ; name : string option
     }
@@ -15,18 +15,18 @@ module File = struct
     let announce = bencode_to_string bencode_root "announce" in
     let info_beencode = Bencode.dict_get bencode_root "info" |> Option.get in
     let length = bencode_to_int info_beencode "length" in
-    let pieceLength = bencode_to_int info_beencode "piece length" in
+    let piece_length = bencode_to_int info_beencode "piece length" in
     let pieces = Bencode.dict_get info_beencode "pieces" |> Option.get in
     let name = bencode_to_string info_beencode "name" in
-    let infoHash = sha1_of_bencode info_beencode in
-    let pieceHashes = split_piece_hashes pieces in
-    { announce; infoHash; pieceHashes; pieceLength; length; name }
+    let info_hash = sha1_of_bencode info_beencode in
+    let piece_hashes = split_piece_hashes pieces in
+    { announce; info_hash; piece_hashes; piece_length; length; name }
   ;;
 
   let build_tracker_url file peer_id port =
     let announce_url = file.announce |> Option.get in
     let query =
-      [ "info_hash", [ Bytes.to_string file.infoHash ]
+      [ "info_hash", [ Bytes.to_string file.info_hash ]
       ; "peer_id", [ Bytes.to_string peer_id ]
       ; "port", [ Int.to_string port ]
       ; "uploaded", [ "0" ]
