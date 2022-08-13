16b66f2728a2bb08ef48970b87fd77ca0df78f6d
Felipe Cardozo <fraifelipe@gmail.com>
2022-08-13 03:24:40 -0300

    add to string msgs

--- lib/message.ml
+++ lib/message.ml
@@ -1,4 +1,4 @@
-(* open Stdint *)
+open Stdint
 
 type message_id =
   | Msg_choke
@@ -13,26 +13,58 @@ type message_id =
 
 let id_of_message_type msg =
   match msg with
-  | Msg_choke -> 0
-  | Msg_unchoke -> 1
-  | Msg_interested -> 2
-  | Msg_not_interested -> 3
-  | Msg_have -> 4
-  | Msg_bitfield -> 5
-  | Msg_request -> 6
-  | Msg_piece -> 7
-  | Msg_cancel -> 8
+  | Msg_choke -> Uint8.of_int 0
+  | Msg_unchoke -> Uint8.of_int 1
+  | Msg_interested -> Uint8.of_int 2
+  | Msg_not_interested -> Uint8.of_int 3
+  | Msg_have -> Uint8.of_int 4
+  | Msg_bitfield -> Uint8.of_int 5
+  | Msg_request -> Uint8.of_int 6
+  | Msg_piece -> Uint8.of_int 7
+  | Msg_cancel -> Uint8.of_int 8
 ;;
 
-type message =
-  { id : message_id
+type t =
+  { id : uint8
   ; payload : bytes
   }
 
+let to_string msg =
+  match msg with
+  | Some msg ->
+    let msg_str =
+      match Uint8.to_int msg.id with
+      | 0 -> "Choke"
+      | 1 -> "Unchoke"
+      | 2 -> "Interested"
+      | 3 -> "NotInterested"
+      | 4 -> "Have"
+      | 5 -> "Bitfield"
+      | 6 -> "Request"
+      | 7 -> "Piece"
+      | 8 -> "Cancel"
+      | id -> Printf.sprintf "Unknown#%d" id
+    in
+    Printf.sprintf "%s [%d]" msg_str (Bytes.length msg.payload)
+  | None -> "KeepAlive"
+;;
+
 let format_request index start length =
   let payload = Bytes.create 12 in
-  Bytes.set_int32_be payload 0 index;
-  Bytes.set_int32_be payload 4 start;
-  Bytes.set_int32_be payload 8 length;
-  { id = Msg_request; payload }
+  Uint32.to_bytes_big_endian index payload 0;
+  Uint32.to_bytes_big_endian start payload 4;
+  Uint32.to_bytes_big_endian length payload 8;
+  { id = id_of_message_type Msg_request; payload }
 ;;
+
+let format_have index =
+  let payload = Bytes.create 4 in
+  Uint32.to_bytes_big_endian index payload 0;
+  { id = id_of_message_type Msg_have; payload }
+;;
+
+(* let format_have index = *)
+(*   let payload = Bytes.create 4 in *)
+(*   Uint32.to_bytes_big_endian index payload 0; *)
+(*   { id = id_of_message_type Msg_have; payload } *)
+(* ;; *)
--- test/unit/message_test.ml
+++ test/unit/message_test.ml
@@ -1,19 +1,120 @@
 open Shared
 open Tornado
+open Stdint
 
 let format_request_test () =
   let expected =
     [ 0x00; 0x00; 0x00; 0x04; (* Index *) 0x00; 0x00; 0x02; 0x37; (* Begin *)
-      0x00; 0x00; 0x10; 0xe1 (* Length *) ] |> Utils.ints_to_bytes
+      0x00; 0x00; 0x10; 0xe1 (* Length *) ]
+    |> Utils.ints_to_bytes
   in
-  let index = Int32.of_int 4 in
-  let start = Int32.of_int 567 in
-  let length = Int32.of_int 4321 in
+  let index = Uint32.of_int 4 in
+  let start = Uint32.of_int 567 in
+  let length = Uint32.of_int 4321 in
   let msg = Message.format_request index start length in
   Check.check_bytes expected msg.payload
 ;;
 
+let format_have_test () =
+  let expected = [ 0x00; 0x00; 0x00; 0x04 ] |> Utils.ints_to_bytes in
+  let index = Uint32.of_int 4 in
+  let msg = Message.format_have index in
+  Check.check_bytes expected msg.payload
+;;
+
+type to_string_case =
+  { input : Message.t option
+  ; expected : string
+  }
+
+let to_string_tests =
+  let cases =
+    [ { input =
+          Some
+            { id = Uint8.of_int 0
+            ; payload = [ 1; 2; 3 ] |> Utils.ints_to_bytes
+            }
+      ; expected = "Choke [3]"
+      }
+    ; { input =
+          Some
+            { id = Uint8.of_int 1
+            ; payload = [ 1; 2; 3 ] |> Utils.ints_to_bytes
+            }
+      ; expected = "Unchoke [3]"
+      }
+    ; { input =
+          Some
+            { id = Uint8.of_int 2
+            ; payload = [ 1; 2; 3 ] |> Utils.ints_to_bytes
+            }
+      ; expected = "Interested [3]"
+      }
+    ; { input =
+          Some
+            { id = Uint8.of_int 3
+            ; payload = [ 1; 2; 3 ] |> Utils.ints_to_bytes
+            }
+      ; expected = "NotInterested [3]"
+      }
+    ; { input =
+          Some
+            { id = Uint8.of_int 4
+            ; payload = [ 1; 2; 3 ] |> Utils.ints_to_bytes
+            }
+      ; expected = "Have [3]"
+      }
+    ; { input =
+          Some
+            { id = Uint8.of_int 5
+            ; payload = [ 1; 2; 3 ] |> Utils.ints_to_bytes
+            }
+      ; expected = "Bitfield [3]"
+      }
+    ; { input =
+          Some
+            { id = Uint8.of_int 6
+            ; payload = [ 1; 2; 3 ] |> Utils.ints_to_bytes
+            }
+      ; expected = "Request [3]"
+      }
+    ; { input =
+          Some
+            { id = Uint8.of_int 7
+            ; payload = [ 1; 2; 3 ] |> Utils.ints_to_bytes
+            }
+      ; expected = "Piece [3]"
+      }
+    ; { input =
+          Some
+            { id = Uint8.of_int 8
+            ; payload = [ 1; 2; 3 ] |> Utils.ints_to_bytes
+            }
+      ; expected = "Cancel [3]"
+      }
+    ; { input =
+          Some
+            { id = Uint8.of_int 99
+            ; payload = [ 1; 2; 3 ] |> Utils.ints_to_bytes
+            }
+      ; expected = "Unknown#99 [3]"
+      }; { input = None; expected = "KeepAlive" } ]
+  in
+  List.map
+    (fun c ->
+      Alcotest.test_case
+        ("To string message: " ^ c.expected)
+        `Quick
+        (fun () ->
+          let msg_str = Message.to_string c.input in
+          Check.check_string c.expected msg_str))
+    cases
+;;
+
 let tests =
   let open Alcotest in
-  "Message", [ test_case "Format request." `Quick format_request_test ]
+  ( "Message"
+  , [ test_case "Format request." `Quick format_request_test
+    ; test_case "Format have." `Quick format_have_test ]
+    @ to_string_tests )
 ;;
