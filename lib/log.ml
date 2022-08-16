f0bc82383ebd06bcf99bd570b47e15aaceff3bad
Felipe Cardozo <fraifelipe@gmail.com>
2022-08-16 00:33:47 -0300

    move log setup to log module

---
+++ lib/log.ml
@@ -0,0 +1,28 @@
+let setup_log ?style_renderer level =
+  let pp_header src ppf (l, h) =
+    if l = Logs.App
+    then Format.fprintf ppf "%a" Logs_fmt.pp_header (l, h)
+    else (
+      let x =
+        match Array.length Sys.argv with
+        | 0 -> Filename.basename Sys.executable_name
+        | _n -> Filename.basename Sys.argv.(0)
+      in
+      let x =
+        if Logs.Src.equal src Logs.default then x else Logs.Src.name src
+      in
+      Format.fprintf ppf "%s: %a " x Logs_fmt.pp_header (l, h))
+  in
+  let format_reporter =
+    let report src =
+      let { Logs.report } = Logs_fmt.reporter ~pp_header:(pp_header src) () in
+      report src
+    in
+    { Logs.report }
+  in
+  Fmt_tty.setup_std_outputs ?style_renderer ();
+  Logs.set_level ~all:true level;
+  Logs.set_reporter format_reporter
+;;
+
+
